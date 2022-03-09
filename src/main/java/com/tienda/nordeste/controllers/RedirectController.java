0c1687c98a58dde402e5d2b200205242ea439655
mdam101 <dam01.2020.jesuitas@gmail.com>
2022-03-09 21:17:46 +0100

    tablas ropa y categoria

--- src/main/java/com/tienda/nordeste/controllers/RedirectController.java
+++ src/main/java/com/tienda/nordeste/controllers/RedirectController.java
@@ -1,7 +1,5 @@
 package com.tienda.nordeste.controllers;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,13 +7,11 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @RestController
-@Api(tags = "Redirect")
 public class RedirectController {
 
     @GetMapping("/")
-    @ApiOperation("Redireccion a la pagina de Swagger")
     public void redirectToSwagger(HttpServletResponse response) throws IOException {
-        response.sendRedirect("swagger-ui.html#/");
+        response.sendRedirect("swagger-ui/index.html");
     }
 
 }
