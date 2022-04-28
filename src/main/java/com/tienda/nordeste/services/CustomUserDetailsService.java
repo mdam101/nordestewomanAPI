61e3a23274474a0ac766aa12503d486d177cf439
mdam101 <dam01.2020.jesuitas@gmail.com>
2022-04-28 19:56:10 +0200

    Tercera tabla talla y ropa

--- src/main/java/com/tienda/nordeste/services/CustomUserDetailsService.java
+++ src/main/java/com/tienda/nordeste/services/CustomUserDetailsService.java
@@ -10,7 +10,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 
-@Service("userDetailsService")
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
     @Autowired
     private UsuarioService usuarioService;
